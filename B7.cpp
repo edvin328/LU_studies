a359001ce63f9fb54e101634b1ed4428dcadc365
Edvins Skrebelis <69343076+edvin328@users.noreply.github.com>
2021-11-03 09:00:28 +0200

    Update B7.cpp

--- B7.cpp
+++ B7.cpp
@@ -4,7 +4,6 @@ using namespace std;
 // Check function: checks if inputted number is in required range
 int check(){
     cout << "Hi, this is c++ program for finding all rational numbers in range [0,1] for which denominator doesn't exceed n" <<endl;
-    // n variable means number of terms in Fibonacci sequence
     int n;
     cout << "Please enter the natural number n<10:";
     cin >> n;
